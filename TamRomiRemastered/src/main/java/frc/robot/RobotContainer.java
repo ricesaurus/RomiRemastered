b692e08908e0875357f0bbcdf6c1ddae7690ece1
ricesaurus <ddtamwarner@gmail.com>
2023-01-26 13:25:32 -0800

    e

---
+++ TamRomiRemastered/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.sensor.RomiDrivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+  //Should these two commands below have m_ prefix? 
+  private final StraightLine_AButton m_straightLine = new StraightLine_AButton(m_romiDrivetrain,24);
+  private final Turn90_BButton m_turnRight = new Turn90_BButton(m_romiDrivetrain);
+  private DriveWithController m_controldrive = new DriveWithController(m_romiDrivetrain);
+  private final Forward m_forward = new Forward(m_romiDrivetrain,5);
+  //end of new stuff / testing
+
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
+
+  //Xboxcontroller object for DriveWithController file
+  public static final XboxController m_xboxController = new XboxController(0);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    //Kevin showed this example not understood yet
+     m_romiDrivetrain.setDefaultCommand(m_controldrive);
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An Exam(pleCommand will run in autonomous
+    //return new Forward(m_romiDrivetrain,10).andThen(new Turn90(m_romiDrivetrain)).andThen(new Forward(m_romiDrivetrain,10)).andThen(new Turn90(m_romiDrivetrain)).andThen(new Forward(m_romiDrivetrain,10).andThen(new Turn90(m_romiDrivetrain)).andThen(new Forward(m_romiDrivetrain, 10)));
+    return m_controldrive;
+  }
+}
---
+++ TamRomiRemastered/src/main/java/frc/robot/sensor/RomiDrivetrain.java
@@ -0,0 +1,99 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+package frc.robot.sensor;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+
+public class RomiDrivetrain extends SubsystemBase {
+  private static final double kCountsPerRevolution = 1440.0;
+  private static final double kWheelDiameterInch = 2.75591; // 70 mm
+
+  // The Romi has the left and right motors set to
+  // PWM channels 0 and 1 respectively
+  private final Spark m_leftMotor = new Spark(0);
+  private final Spark m_rightMotor = new Spark(1);
+
+  // The Romi has onboard encoders that are hardcoded
+  // to use DIO pins 4/5 and 6/7 for the left and right
+  private final Encoder m_leftEncoder = new Encoder(4, 5);
+  private final Encoder m_rightEncoder = new Encoder(6, 7);
+
+  // Set up the differential drive controller
+  private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+//Set Kp to 0.1, the rest to 0
+  public final PIDController PID = new PIDController(0.1,0,0);
+  public final RomiGyro m_gyroscope = new RomiGyro();
+
+
+  /** Creates a new RomiDrivetrain. */
+  public RomiDrivetrain() {
+    // Use inches as unit for encoder distances
+    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    resetEncoders();
+
+    // Invert right side since motor is flipped
+    m_rightMotor.setInverted(true);
+  }
+
+  
+
+  public double getLeftDistanceInch() {
+    return m_leftEncoder.getDistance();
+  }
+
+  public double getRightDistanceInch() {
+    return m_rightEncoder.getDistance();
+  }
+  public double getAverageDistanceInch(){
+    return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance())/2;
+  }
+//get Gyro angle made accessible through RomiDrivetrain object 
+  public double getGyroAngle(){
+    return m_gyroscope.getAngleZ();
+  }
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+  }
+
+  public void resetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
+  //method to reset gyro made accessible through RomiDrivetrain object
+  public void resetGyro(){
+    m_gyroscope.reset();
+  }
+
+  public void straightLineWithPID(){
+    double pidAdjust = PID.calculate(-m_gyroscope.getAngleZ(),0);
+    m_diffDrive.arcadeDrive(0.7, pidAdjust);
+
+
+  }
+  
+  public void turnRightWithPID(){
+    //PID calculate to adjust to 90 degree angle 
+    //assuming 90 degrees is right
+    double pidAdjust = PID.calculate(-m_gyroscope.getAngleZ(),90);
+    m_diffDrive.arcadeDrive(0,pidAdjust);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
